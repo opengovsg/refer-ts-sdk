d8ce26c204501201c86b6f982c652b93f0df9e25
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-12-17 03:45:11 +0000

    Release 0.0.12

--- src/api/resources/health/client/Client.ts
+++ src/api/resources/health/client/Client.ts
@@ -47,8 +47,8 @@ export class Health {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@opengovsg/refx-ts-sdk",
-                "X-Fern-SDK-Version": "0.0.11",
-                "User-Agent": "@opengovsg/refx-ts-sdk/0.0.11",
+                "X-Fern-SDK-Version": "0.0.12",
+                "User-Agent": "@opengovsg/refx-ts-sdk/0.0.12",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
