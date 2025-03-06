be2be93c4b2260fb6a52bc391ae9c50d00f140f1
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-06 02:47:26 +0000

    Release 0.0.28

--- src/api/resources/offerings/client/Client.ts
+++ src/api/resources/offerings/client/Client.ts
@@ -47,8 +47,8 @@ export class Offerings {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@opengovsg/refx-ts-sdk",
-                "X-Fern-SDK-Version": "0.0.24",
-                "User-Agent": "@opengovsg/refx-ts-sdk/0.0.24",
+                "X-Fern-SDK-Version": "0.0.28",
+                "User-Agent": "@opengovsg/refx-ts-sdk/0.0.28",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -116,8 +116,8 @@ export class Offerings {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@opengovsg/refx-ts-sdk",
-                "X-Fern-SDK-Version": "0.0.24",
-                "User-Agent": "@opengovsg/refx-ts-sdk/0.0.24",
+                "X-Fern-SDK-Version": "0.0.28",
+                "User-Agent": "@opengovsg/refx-ts-sdk/0.0.28",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
