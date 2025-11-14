4573ca9a0e4d80bd6aa5aa0e786911dd62e01292
zhengzheng-jason <zheng@open.gov.sg>
2025-11-14 11:51:17 +0800

    chore: export error as wildcard

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * as ReferralExchange from "./api";
 export { ReferralExchangeClient } from "./Client";
 export { ReferralExchangeJwtClient } from "./wrapper/ReferralExchangeJwtClient";
 export { ReferralExchangeEnvironment } from "./environments";
-export { ReferralExchangeError, ReferralExchangeTimeoutError } from "./errors";
+export * from "./errors";
